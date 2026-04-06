4a6fdf29a0786f98c4fbbba33c49d5aa77f78171
ema <mccoy39082@163.com>
2026-04-07 01:49:44 +0800

    Move BackdropType enum to its own file

---
+++ src/Backdrop/BackdropType.cs
@@ -0,0 +1,11 @@
+﻿namespace System.Backdrop;
+
+public enum BackdropType
+{
+    None = 1,
+    Mica = 2,
+    Acrylic = 3, // Automatically Acrylic. Automatically selects the best Acrylic effect available on the system (Acrylic11 > Acrylic10)
+    Tabbed = 4, // Mica Alt
+    Acrylic10 = 5, // Acrylic. Windows 10 style, supported on Windows 10 and 11
+    Acrylic11 = 6, // Desktop Acrylic. Windows 11 style, supported on Windows 11 22523+ (Insider) and 22621+ (Stable)
+}
